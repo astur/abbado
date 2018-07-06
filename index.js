f61fdf9fafc75dc55d0d2c497df124a4c2eb6050
astur <astur@yandex.ru>
2018-07-06 19:39:37 +0300

    stub of 'wait' method

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ module.exports = ({
     timeout = null,
     count = null,
 } = {}) => {
+    // stop
     let off = false;
-
     if(timeout){
         setTimeout(() => {
             off = true;
         }, timeout);
     }
-
     const stop = () => {
         off = true;
     };
-
     const stopped = () => off || count !== null && count-- <= 0;
 
-    return {stop, stopped};
+    // pause
+    const wait = () => new Promise(resolve => {
+        resolve();
+    });
+
+    return {stop, stopped, wait};
 };
