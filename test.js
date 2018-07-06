8044c3b5108c56051a4c6d4318aec26fa1b61d71
astur <astur@yandex.ru>
2018-07-06 17:11:35 +0300

    test for manual stop

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const test = require('ava');
 const m = require('.');
 
-test('abbado', t => {
-    t.true(true);
-    t.is(m, m);
+test('stop manually', t => {
+    const _ = m();
+    t.is(_.stopped(), false);
+    _.stop();
+    t.is(_.stopped(), true);
 });
