1603c3ede6b71a53ce5beb38a2f30f2624c073fa
astur <astur@yandex.ru>
2018-07-06 17:37:32 +0300

    test for stop by timeout

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
 const test = require('ava');
 const m = require('.');
+const delay = require('delay');
 
 test('stop manually', t => {
     const _ = m();
     t.is(_.stopped(), false);
     _.stop();
+    t.is(_.stopped(), true);
+});
+
+test('stop by timeout', async t => {
+    const _ = m({timeout: 30});
+    t.is(_.stopped(), false);
+    await delay(20);
+    t.is(_.stopped(), false);
+    await delay(20);
     t.is(_.stopped(), true);
 });
